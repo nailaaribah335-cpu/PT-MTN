f6999e5762c6d00aeff57ff946b68df28dbca88f
nailaaa <naiizah113@gmail.com>
2026-05-27 11:08:57 +0700

    BARU BANGET

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Pastikan baris ini ada
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Paksa aplikasi menggunakan HTTPS jika berjalan di server cloud (Railway)
+        if (config('app.env') === 'production' || env('ASSET_URL')) {
+            URL::forceScheme('https');
+        }
     }
 }
